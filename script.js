379324ae0be1472b72e8c6f5abc4b616e0cf7892
Natalya Ivanova <natalyas.ivanova@gmail.com>
2018-04-02 15:21:14 +0300

    Cats list and container to display single cat

--- script.js
+++ script.js
@@ -4,61 +4,64 @@ document.addEventListener('DOMContentLoaded', function() {
     var cats = [
         {
             name: 'Tom',
-            url: 'https://www.cats.org.uk/uploads/images/featurebox_sidebar_kids/grief-and-loss.jpg'
+            url: 'https://www.cats.org.uk/uploads/images/featurebox_sidebar_kids/grief-and-loss.jpg',
+            count: 0,
         },
         {
             name: 'Kitty',
-            url: 'http://a57.foxnews.com/images.foxnews.com/content/fox-news/lifestyle/2017/11/09/how-to-keep-cat-from-scratching-your-sofa-to-shreds/_jcr_content/par/featured_image/media-0.img.jpg/931/524/1510172827500.jpg?ve=1&tl=1&text=big-top-image'
+            url: 'http://a57.foxnews.com/images.foxnews.com/content/fox-news/lifestyle/2017/11/09/how-to-keep-cat-from-scratching-your-sofa-to-shreds/_jcr_content/par/featured_image/media-0.img.jpg/931/524/1510172827500.jpg?ve=1&tl=1&text=big-top-image',
+            count: 0,
+        },
+        {
+            name: 'Chess',
+            url: 'https://static.boredpanda.com/blog/wp-content/uploads/2017/01/two-faced-cat-yana-28.jpg',
+            count: 0,
+        },
+        {
+            name: 'Gipsy',
+            url: 'https://pbs.twimg.com/profile_images/378800000532546226/dbe5f0727b69487016ffd67a6689e75a_400x400.jpeg',
+            count: 0,
+        },
+        {
+            name: 'Snow-White',
+            url: 'http://r.ddmcdn.com/s_f/o_1/cx_462/cy_245/cw_1349/ch_1349/w_720/APL/uploads/2015/06/caturday-shutterstock_149320799.jpg',
+            count: 0,
         }
     ];
 
-    function Cat(name, img) {
-        this.name = name;
-        this.img = img;
-        this.count = 0;
+    const list = document.querySelector('ul');
+    const container = document.querySelector('.cat');
+    let selected = 0;
 
-        function renderName() {
-            this.nameElement = document.createElement('h2');
-            this.nameElement.innerText = this.name;
-        }
+    const nameElement = document.createElement('h2');
+    container.appendChild(nameElement);
+    const imageElement = document.createElement('img');
+    container.appendChild(imageElement);
+    const clicks = document.createElement('div');
+    clicks.className = 'count';
+    container.appendChild(clicks);
 
-        function renderImage() {
-            this.imageElement = document.createElement('img');
-            this.imageElement.setAttribute('src', this.img);
-        }
+    imageElement.addEventListener('click', function() {
+        cats[selected].count++;
+        clicks.innerText = cats[selected].count; 
+    });
 
-        function makeClickable() {
-            this.clicks = document.createElement('div');
-            this.clicks.innerText = this.count;
-            this.clicks.className = 'count';
-            const clicks = this.clicks;
-            let count = this.count;
-            this.imageElement.addEventListener('click', function() {
-                count ++;
-                clicks.innerText = count;
-            });
-        }
-
-        function makeAlive() {
-            this.container = document.createElement('div');
-            this.container.className = 'cat';
-            this.renderName();
-            this.renderImage();
-            this.container.appendChild(this.nameElement);
-            this.container.appendChild(this.imageElement);
-            this.makeClickable();
-            this.container.appendChild(this.clicks);
-            document.body.appendChild(this.container);
-        }
-
-        this.renderName = renderName;
-        this.renderImage = renderImage;
-        this.makeClickable = makeClickable;
-        this.makeAlive = makeAlive;
+    function displayCat(i) {
+        selected = i;
+        nameElement.innerText = cats[i].name;
+        imageElement.setAttribute('src', cats[i].url);
+        clicks.innerText = cats[i].count;            
     }
 
-    for (var i = 0; i < cats.length; i++) {
-        var cat = new Cat(cats[i].name, cats[i].url);
-        cat.makeAlive();
+
+    for (let i = 0; i < cats.length; i++) {
+        const listItem = document.createElement('li');
+        listItem.innerText = cats[i].name;
+        list.appendChild(listItem);
+        listItem.addEventListener('click', function() {
+            displayCat(i);
+        })
     }
+
+    displayCat(0);
 });
